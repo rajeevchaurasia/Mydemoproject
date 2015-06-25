6ace0347319cbe5caf2ae026d5d1d734132d294a
leena <leebhise92@gmail.com>
2015-06-25 16:45:21 +0530

    change in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625101937) do
+ActiveRecord::Schema.define(version: 20150625111054) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.integer  "discription"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
