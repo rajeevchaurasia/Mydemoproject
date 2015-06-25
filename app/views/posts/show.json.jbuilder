07192f5245edf7771feda93c136baeef9044a247
leena <leebhise92@gmail.com>
2015-06-25 16:29:48 +0530

    first commit

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :title, :discription, :user_id, :created_at, :updated_at
---
+++ db/migrate/20150625101937_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.integer :discription
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
